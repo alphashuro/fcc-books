8581e7a1a5dcf49693bb280ea8c241bee0095187
Alpha Shuro <alphashuro@gmail.com>
2016-06-26 12:13:56 +0200

    update app for router

--- app/components/app.js
+++ app/components/app.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-const App = () => (
-	<div>App</div>
+// theme required by material ui
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+
+const App = ({user, children}) => (
+	<MuiThemeProvider>
+		<div className="app">
+			<AppBar
+				title="Bookclub"
+				showMenuIconButton={false}
+			/>
+			{ children }
+		</div>
+	</MuiThemeProvider>
 );
 
 export default App;
