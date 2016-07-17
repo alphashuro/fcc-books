c97caac336f870d82ee866ea979ed7554380fcc0
Alpha Shuro <alphashuro@gmail.com>
2016-07-17 21:07:59 +0200

    add google signin

--- app/redux/actions/signin.js
+++ app/redux/actions/signin.js
@@ -8,36 +8,21 @@ export const signinRequest = () => ({
 export const signinSuccess = ({user, token}) => ({
 	type: types.SIGNIN_SUCCESS,
 	user,
-	token,
 });
 export const signinFailure = error => ({
 	type: types.SIGNIN_FAILURE,
 	error,
 });
 
-export const signin = (email, password) => async dispatch => {
+export const signinGoogle = () => async dispatch => {
 	dispatch(signinRequest());
 
-	const options = {
-		method: 'POST',
-		headers: {
-			'Accept': '*',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			email, password
-		}),
-	};
-
 	try {
-		const response = await fetch(apiUrl+'/signin', options);
-		const {user, token} = await response.json();
-		dispatch(signinSuccess({user, token}));
-		dispatch(push('/'));
-	} catch (e) {
-		console.error(e);
-		dispatch(signinFailure(e.message || e));
+		const provider = new firebase.auth.GoogleAuthProvider();
+		const {user} = await firebase.auth().signInWithPopup(provider)
+	  dispatch(signinSuccess({user}));
+	} catch (error) {
+		console.error(error);
+		dispatch(signinFailure(error));
 	}
-};
-
-export default signin;
+}
